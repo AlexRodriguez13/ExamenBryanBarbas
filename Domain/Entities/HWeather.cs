1c4230cc507151978457ad191b57c52ef0e82eac
AlexRodriguez13 <bryanalexandercb13@gmail.com>
2022-05-04 09:51:17 -0600

    class current inside weather class

--- Domain/Entities/HWeather.cs
+++ Domain/Entities/HWeather.cs
@@ -10,8 +10,31 @@ namespace Domain.Entities
     {
         public class Weather
         {
-            public int id
-            public int
+            public int id { get; set; } 
+            public string main { get; set; }
+            public string description { get; set; }
+            public string icon { get; set; }
         }
+
+        public class Current
+        {
+            public int dt { get; set; }
+            public int sunrise { get; set; }
+            public int sunset { get; set; }
+            public double temp { get; set; }
+            public double feels_like { get; set; }
+            public int pressure { get; set; }
+            public int humidity { get; set; }
+            public double dew_point { get; set; }
+            public int uvi { get; set; }
+            public int clouds { get; set; }
+            public int visibility { get; set; }
+            public double wind_speed { get; set; }
+            public int wind_deg { get; set; }
+            public double wind_gust { get; set; }
+            public List<Weather> weather { get; set; }
+        }
+
+
     }
 }
