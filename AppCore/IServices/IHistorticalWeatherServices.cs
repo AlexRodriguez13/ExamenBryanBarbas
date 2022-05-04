1aacada0d390f739b449828fc0503a9c043733fc
AlexRodriguez13 <bryanalexandercb13@gmail.com>
2022-05-04 09:20:01 -0600

    primer intento de interfaz

---
+++ AppCore/IServices/IHistorticalWeatherServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCore.IServices
+{
+    public interface IHistorticalWeatherServices : IServices<HistoricalWeather>
+    {
+        HistoricalWeather GetById(int id);
+        void Update(HistoricalWeather t);
+    }
+}
