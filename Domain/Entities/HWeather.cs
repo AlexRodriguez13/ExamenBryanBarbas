88f5e26229b84b5670ddff83dc260e07bfef1783
AlexRodriguez13 <bryanalexandercb13@gmail.com>
2022-05-04 09:54:02 -0600

    class snow & hourly inside weather class

--- Domain/Entities/HWeather.cs
+++ Domain/Entities/HWeather.cs
@@ -35,6 +35,30 @@ namespace Domain.Entities
             public List<Weather> weather { get; set; }
         }
 
+        public class Snow
+        {
+            public double _1h { get; set; }
+        }
+
+        public class Hourly
+        {
+            public int dt { get; set; }
+            public double temp { get; set; }
+            public double feels_like { get; set; }
+            public int pressure { get; set; }
+            public int humidity { get; set; }
+            public double dew_point { get; set; }
+            public double uvi { get; set; }
+            public int clouds { get; set; }
+            public int visibility { get; set; }
+            public double wind_speed { get; set; }
+            public int wind_deg { get; set; }
+            public double wind_gust { get; set; }
+            public List<Weather> weather { get; set; }
+            public Snow snow { get; set; }
+        }
+
+
 
     }
 }
