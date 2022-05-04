1bacde335e5d56500d0315ed1dc520b408474735
AlexRodriguez13 <bryanalexandercb13@gmail.com>
2022-05-04 09:24:43 -0600

    carpeta Iservices

---
+++ AppCore/IServices/IHttpHistoricalWeatherServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCore.IServices
+{
+    public interface IHttpHistoricalWeatherServices
+    {
+        Task<HistoricalWeather> GetWeatherByCityNameAsync(string lat, string lon, string time);
+    }
+
+}
---
+++ AppCore/IServices/IServices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCore.IServices
+{
+    public interface IServices<T>
+    {
+
+        void Add(T t);
+        bool Delete(int id);
+        List<T> Read();
+    }
+}
