94183d4fa4444859b9c225639e1f36c4f263dcde
AlexRodriguez13 <bryanalexandercb13@gmail.com>
2022-05-04 09:44:39 -0600

    Class Weather en el domain

---
+++ Common/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+   public class AppSettings
+    {
+        public static string ApiUrl { get => ConfigurationManager.AppSettings.Get("ApiUrl"); }
+        public static string Token { get => ConfigurationManager.AppSettings.Get("Token"); }
+    }
+}
